fbbd0c3ace9e3b3de4f9b1010b8fbe942584283b
Artem Maltsev <artemmaltsevwork@gmail.com>
2025-11-20 15:43:26 +0200

    Added admin menu with adding products in localStorage

--- js/app.js
+++ js/app.js
@@ -3,5 +3,78 @@ import { Entity } from "./entity.js"
 import { Status } from "./status.js"
 import { User } from "./user.js"
 
-let category = new Category(new Entity(1), "test123");
-category.showName();
+class Product {
+  constructor(name, category, cost) {
+    this.name = name;
+    this.category = category;
+    this.cost = cost;
+  }
+}
+
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + JSON.stringify(value) + expires + "; path=/; SameSite=Lax";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    cookie = cookie.trim();
+    if (cookie.startsWith(nameEQ)) {
+      const cookieValue = cookie.substring(nameEQ.length);
+      return JSON.parse(cookieValue);
+    }
+  }
+}
+
+function getProducts() {
+  const products = getCookie("items");
+  console.log(products);
+  return products ? products : [];
+}
+
+function displayProducts() {
+  const products = getProducts();
+  const productsList = document.getElementById("productsList");
+
+  if (products.length === 0) {
+    productsList.innerHTML = "<p>Немає товарів</p>";
+    return;
+  }
+
+  let products_text = ''
+
+  products.forEach(product => {
+    products_text += `
+      <div class="product">
+        <div>Назва товару: ${product.name}</div>
+        <div>Категорія: ${product.category}</div>
+        <div>Ціна: ${product.cost} грн</div>
+      </div>
+    `;
+  });
+
+  productsList.innerHTML = products_text;
+}
+
+document.getElementById('productForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const category = document.getElementById('category').value;
+  const cost = document.getElementById('cost').value;
+  const newProduct = new Product(name, category, cost);
+  const products = getProducts();
+  products.push(newProduct);
+  setCookie("items", products);
+  displayProducts();
+  this.reset();
+});
+
+displayProducts();
--- js/product.js
+++ js/product.js
@@ -1,9 +1,10 @@
 import { Entity } from "./entity.js";
 
 export class Product extends Entity {
-  constructor(id, name, description, price, category, supplier) {
+  constructor(id, name, image_url, description, price, category, supplier) {
     super(id);
     this.name = name;
+    this.image_url = image_url;
     this.description = description;
     this.price = price;
     this.category = category;
